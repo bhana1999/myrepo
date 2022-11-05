68b6d285f5a73fc00099483089ed43a545893191
bhana1999 <bhawanachaudhary333@gmail.com>
2022-11-05 23:33:40 +0530

    authentication assig

---
+++ src1/controllers/userController.js
@@ -0,0 +1,84 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel");
+
+/*
+  Read all the comments multiple times to understand why we are doing what we are doing in login api and getUserData api
+*/
+const createUser = async function (req, res) {
+  //You can name the req, res objects anything.
+  //but the first parameter is always the request 
+  //the second parameter is always the response
+  let data = req.body;
+  let savedData = await userModel.create(data);
+  console.log(req.newAtribute);
+  res.send({ msg: savedData });
+};
+
+const loginUser = async function (req, res) {
+  let userName = req.body.emailId;
+  let password = req.body.password;
+
+  let user = await userModel.findOne({ emailId: userName, password: password });
+  if (!user)
+    return res.send({
+      status: false,
+      msg: "username or the password is not corerct",
+    });
+
+  // Once the login is successful, create the jwt token with sign function
+  // Sign function has 2 inputs:
+  // Input 1 is the payload or the object containing data to be set in token
+  // The decision about what data to put in token depends on the business requirement
+  // Input 2 is the secret (This is basically a fixed value only set at the server. This value should be hard to guess)
+  // The same secret will be used to decode tokens 
+  let mytoken = jwt.sign(
+    {
+      userId: user._id.toString(),
+      batch: "lithium",
+      organisation: "FunctionUp",
+    },
+    "functionup-lithium-very-very-secret-key"
+  );
+  res.setHeader("x-auth-token", mytoken);
+  res.send({ status: true, token: mytoken });
+};
+
+const getUserData = async function (req, res) {
+  let userId = req.params.userId
+  let token = req.headers["x-auth-token"];
+  if (!token)
+  res.send({ status: false, msg: "token must be present" });
+  let decodedToken = jwt.verify(token, "functionup-lithium-very-very-secret-key");
+  if (!decodedToken)
+    res.send({ status: false, msg: "token is invalid" });
+  let userDetails = await userModel.findById({_id:userId})
+  if (!userDetails) res.send({msg : " Invalid userId"})
+  res.send({ status: true, data: userDetails });}
+  
+  const updateUser = async function (req, res) {
+  let userId = req.params.userId;
+  let user = await userModel.findById(userId);
+  if (!user) {
+    return res.send("No such user exists");
+  }
+
+  let userData = req.body;
+  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
+  res.send({ status: updatedUser, data: updatedUser });
+};
+const deleteUser = async function (req, res) {
+  let userId = req.params.userId;
+  let user = await userModel.findById(userId);
+  if (!user) {
+    return res.send("No such user exists");
+  }
+
+  let userData = req.body;
+  let updatedUser = await userModel.findOneAndDelete({ _id: userId }, userData);
+  res.send({ status: updatedUser, data: updatedUser });
+};
+module.exports.createUser = createUser;
+module.exports.getUserData = getUserData;
+module.exports.updateUser = updateUser;
+module.exports.loginUser = loginUser;
+module.exports.deleteUser = deleteUser;
