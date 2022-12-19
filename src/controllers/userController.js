4e4a45f322d947468ea417c6b27ede5da52d784a
ChavanPriya97 <priya971110@gmail.com>
2022-12-19 18:20:18 +0530

    add get user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,63 @@
 const userModel = require("../model/userModel");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const aws =  require("aws-sdk")
 const Validator = require("../validation/validator");
 
+aws.config.update({
+  accessKeyId : "AKIAY3L35MCRZNIRGT6N",
+  secretAccessKey : "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",
+  region: "ap-south-1"
+})
+
+  let uploadFile = async ( file) =>{
+  return new Promise(function(resolve, reject) {
+    let s3= new aws.S3({apiVersion: '2006-03-01'});
+ 
+    var uploadParams= {
+        ACL: "public-read",
+        Bucket: "classroom-training-bucket", 
+        Key: "abc/" + file.originalname, 
+        Body: file.buffer
+    }
+    s3.upload( uploadParams, function (err, data ){
+        if(err) {
+            return reject({"error": err})
+        }
+        console.log(data)
+        console.log("file uploaded succesfully")
+        return resolve(data.Location)
+    })
+  })
+}
+
+const  profileImage = async function(req, res){
+  try{
+      let files= req.files
+      if(files && files.length>0){
+          let uploadedFileURL= await uploadFile( files[0] )
+          res.status(201).send({msg: "file uploaded succesfully", data: uploadedFileURL})
+      }
+      else{
+          res.status(400).send({ msg: "No file found" })
+      }
+      
+  }
+  catch(err){
+      res.status(500).send({msg: err})
+  }
+}
 
 const createUser = async function (req, res) {
     try {
       let data = req.body;
-  
-      const { fname, lname, email, phone, password, address } = data;
+
+      let bearerToken = req.headers.authorization
+      console.log(bearerToken)
+      let temp =  bearerToken.split(" ")
+      let token = temp[1]
+
+      const { fname, lname,email,uploadFile, phone, password, address } = data;
 
  if (!Validator.isValidBody(data)) {
     return res.status(400).send({status: false,message: "User data is required for registration", });
@@ -39,29 +88,30 @@ const createUser = async function (req, res) {
     return res.status(400).send({ status: false, message: "phone number already exist" });
   }
 
-  if (
-    !Validator.isValidInputValue(password) || !Validator.isValidPassword(password)
+  if (!Validator.isValidInputValue(password) || !Validator.isValidPassword(password)
   ) {
     return res.status(400).send({ status: false, message:"Password is required and should be of 8 to 15 characters and  must have atleast 1 number",});
   }
 
-  if (!Validator.isValidAddress(address)) {
-    return res.status(400).send({ status: false, message: "Address is required!" });
+  if(address){
+    const {shipping,billing} = address
+
+    if(shipping){
+      const {street,city,pincode } = shipping
+
+    }
+
+    if(billing){
+      const {street,city,pincode } = billing
+    }
   }
 
-  if (!files || files.length == 0) {
-    return res.status(400).send({ status: false, message: "No profile image found" });
-  }
-
-  if (!Validator.isValidImageType(files[0].mimetype)) {
-    return res.status(400).send({status: false,message: "Only images can be uploaded (jpeg/jpg/png)"});
-  }
-
-  let fileUrl = await uploadFile(files[0].mimetype);
-  data.profileImage = fileUrl;
+  // if (!Validator.isValidImageType(uploadFile)) {
+  //   return res.status(400).send({status: false,message: "Only images can be uploaded (jpeg/jpg/png)"});
+  // }
 
   const saltRounds = 10;
-  let encryptedPassword = bcrypt.hash(data.password, saltRounds)
+  let encryptedPassword = bcrypt.hash(password, saltRounds)
     .then((hash) => { console.log(`Hash: ${hash}`);
     return hash;
     });
@@ -75,5 +125,15 @@ const createUser = async function (req, res) {
     }
 }
 
+const getUser = async function(req,res){
+  try {
+      const userId = req.params.userId
+      const user = await userModel.find({_id :userId})
+      return res.status(200).send({status :  true ,message: "User profile details",data:user})
+      
+  } catch (error) {
+      return res.status(500).send({status : false , message : error.message})     
+  }
+}
 
-module.exports = {createUser}
+module.exports = {profileImage,createUser,getUser}
--- src/router/route.js
+++ src/router/route.js
@@ -2,13 +2,13 @@ const express =require("express")
 const router = express.Router()
 const userContoller = require("../controllers/userController")
 
+/*******************************create image url **********************/
+router.post("/profileImage" ,userContoller.profileImage)
 
-
-
-
+/*******************************create user  *************************/
 router.post('/register',userContoller.createUser)
 
-
-
+/******************************get user *****************************/
+router.get("/user/:userId/profile",userContoller.getUser)
 
 module.exports = router;
