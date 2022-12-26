0eb85064c7d8bad755742fd2da0ea2c485b11a86
ChavanPriya97 <priya971110@gmail.com>
2022-12-26 15:28:22 +0530

    done

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -62,8 +62,46 @@ const productModel = require("../model/productModel");
   
       return res.status(201).send({ status: true, message: "Success", data: finalData,});
     } catch (error) {
-      res.status(500).send({ status: false, message: error.message });
+      return res.status(500).send({ status: false, message: error.message });
     }
   };
 
-  module.exports ={createOrder}
+
+  /**********************************update order  ****************/
+  const updateOrder = async function(req,res){
+    try {
+      let userId = req.params.userId
+      let data = req.body
+      let {status ,orderId } = data
+
+      if(Object.keys(data).length == 0) return res.status(400).send({status: false,message: "Please provide data in request body"});
+
+      // const user = await userModel.findOne({ _id : userId})
+      // if(!user) return res.status(404).send({status : false , message :  "user not found"})
+
+      const findOrder = await orderModel.findOne({ _id : orderId, isDeleted : false})
+      if(!findOrder) return res.status(404).send({status : false , message :  "order not found"})
+
+      if(userId != findOrder.userId) return res.status(403).send({status : false ,message : "user is not authorised user"})
+
+      let statuslist =  ["pending", "completed", "canceled"]
+      if(!statuslist.includes(status)) return res.status(400).send({status : false , message : "status must be pending or completed or canceled"});
+
+      const order = await orderModel.findOneAndUpdate(
+        {_id : orderId , isDeleted : false , cancellable : true},
+        data,
+        {new : true}
+      )
+
+      if(!order) return res.status(404).send({status : false , message :  "order not found"})
+
+      return res.status(200).send({status : true , message :" Order Updated Successfully ",data : order})
+
+      
+    } catch (error) {
+      return res.status(500).send({ status: false, message: error.message });
+      
+    }
+  }
+
+  module.exports ={createOrder ,updateOrder}
